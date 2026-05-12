79d127082bb82d20258d61d7d7a81bee8cae5c1f
snickpick <soda.off21@gmail.com>
2026-05-12 16:33:12 +0530

    models from pydantic

--- src/workflow.py
+++ src/workflow.py
@@ -52,5 +52,73 @@ class Workflow:
             return {"extracted_tools": []}
 
 
-            
+    def _analyze_company_content(self,  company_name: str, content:str) -> CompanyAnalysis:       
+        structured_llm = self.llm.with_structured_output(CompanyAnalysis) 
 
+        messages = [
+                SystemMessage(content=self.prompts.TOOL_ANALYSIS_SYSTEM),
+                HumanMessage(content=self.prompts.tool_analysis_user(company_name, content))
+        ]
+
+        try:
+            analysis = structured_llm.invoke(messages)
+            return analysis
+        except Exception as e:
+            print("Error: ", e)
+            return CompanyAnalysis(
+                    pricing_model="Unknown",
+                    is_open_source=None,
+                    tech_stack=[],
+                    description="Failed",
+                    api_available=None,
+                    language_support=[],
+                    integration_capabilites=[],
+            )
+
+    def _research_step(self, state: ResearchState) -> Dict[str, Any]:
+        extracted_tools = getattr(state, "extracted_tools", [])
+
+        if not extracted_tools:
+            print("No extracted tools found, falling back to direct search")
+            search_results = self.firecrawl.search_companies(state.query, num_results=4)
+            tool_names = [
+                    result.get("metadata", {}).get("title", "Unknown")
+                    for result in search_results.data
+            ]
+        else:
+            tool_names = extracted_tools[:4]
+
+        print(f"Researching specific tools: {', '.join(tool_names)}")
+
+        companies = []
+        for tool_name in tool_names:
+            tool_search_results = self.firecrawl.search_companies(tool_name + " official site", num_results=1)
+
+            if tool_search_results:
+                result = tool_search_results.data[0]
+                url = result.get("url", "")
+
+
+                company = CompanyInfo(
+                        name= tool_name,
+                        description= result.get("markdown", ""),
+                        website=url,
+                        tech_stack=[],
+                        competitors=[]
+                )
+
+                scraped = self.firecrawl.scrape_comapany_pages(url)
+                if scraped:
+                    content = scraped.markdown
+                    analysis = self._analyze_company_content(company.name, content)
+
+                    company.pricing_model = analysis.pricing_model
+                    company.is_open_source = analysis.is_open_source
+                    company.description = analysis.description
+                    company.api_available = analysis.api_available
+                    company.language_support = analysis.language_support
+                    company.integration_capabilites = analysis.integration_capabilites
+
+                companies.append(company)
+
+            return {"companies": companies}
